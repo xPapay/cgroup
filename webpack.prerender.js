1881882db85f8449c8f3378e05ef05f7e7ecdcd5
xPapay <papay.lukas@gmail.com>
2019-03-29 13:45:14 +0100

    Use js-dom for prerendering

--- webpack.prerender.js
+++ webpack.prerender.js
@@ -1,24 +1,26 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const commonConfig = require('./webpack.common')
+const Renderer = require('@prerenderer/renderer-jsdom')
 // TODO: Import routes from router
 
 const config = {
     entry: './src/entry-prerender.js',
     output: {
+        path: path.resolve(__dirname, './prerendered'),
         publicPath: '/'
     },
     devtool: 'none',
     mode: 'production',
     plugins: [
         new HTMLWebpackPlugin({
-            template: path.join(__dirname, 'public', 'template.html')
+            template: path.join(__dirname, 'public', 'prerender-template.html')
         }),
         new PrerenderSPAPlugin({
-            staticDir: path.join(__dirname, 'dist'),
+            staticDir: path.join(__dirname, 'prerendered'),
             routes: ['/', '/management-team'],
             renderer: new Renderer({
                 renderAfterDocumentEvent: 'render-event'
